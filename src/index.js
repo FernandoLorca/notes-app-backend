8cadc21d972eeb7f8e79fc0cb579d40dd0083517
FernandoLorca <florcapinto@gmail.com>
2023-08-10 10:18:17 -0400

    Create the schemas for users and notes. Created the relation between users and notes. Users has many notes so the relation was created in that convention

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ dotenv.config();
 
 import app from './app.js';
 import { sequelize } from './database/connection.js';
+import './users/models/users.models.js';
+import './notes/models/notes.models.js';
 
 const PORT = process.env.SERVER_PORT || 3000;
 const main = async () => {
   try {
-    await sequelize.authenticate();
-    console.log('Database connection OK!');
+    // Para probar la conexión a la base de datos:
+    // await sequelize.authenticate();
+    // console.log('Database connection OK!');
+    await sequelize.sync({ force: true });
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   } catch (error) {
     console.error('Unable to connect to the database', error);
