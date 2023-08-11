e30ff22ccdb4760e6e3b36fbfecb334c6d4dafce
FernandoLorca <florcapinto@gmail.com>
2023-08-11 18:51:52 -0400

    Add error code no function errorStatusHandler() in notes controller

--- src/notes/middleware/notes.middlewares.js
+++ src/notes/middleware/notes.middlewares.js
@@ -4,12 +4,12 @@ import { Notes } from '../models/notes.models.js';
 const noteIdVerification = async (req, res, next) => {
   const { id } = req.params;
 
-  if (!id) return res.status(400).json(errorStatusHandler());
+  if (!id) return res.status(400).json(errorStatusHandler(400));
 
   try {
     const note = await Notes.findByPk(id);
 
-    if (!note) return res.status(404).json(errorStatusHandler(403));
+    if (!note) return res.status(404).json(errorStatusHandler(404));
 
     next();
   } catch (error) {
@@ -21,7 +21,7 @@ const hasContentToCreate = (req, res, next) => {
   const { title, content, userId } = req.body;
 
   if (!title || !content || !userId)
-    return res.status(400).json(errorStatusHandler(500));
+    return res.status(400).json(errorStatusHandler(400));
 
   next();
 };
@@ -30,18 +30,18 @@ const hasContentToUpdate = async (req, res, next) => {
   const { id } = req.params;
   const { title, content } = req.body;
 
-  if (!id) return res.status(400).json(errorStatusHandler());
+  if (!id) return res.status(400).json(errorStatusHandler(400));
 
-  if (!title || !content) return res.status(400).json(errorStatusHandler());
+  if (!title || !content) return res.status(400).json(errorStatusHandler(400));
 
   try {
     const note = await Notes.findByPk(id);
 
-    if (!note) return res.status(404).json(errorStatusHandler());
+    if (!note) return res.status(404).json(errorStatusHandler(404));
 
     next();
   } catch (error) {
-    res.status(500).json(errorStatusHandler());
+    res.status(500).json(errorStatusHandler(500));
   }
 };
 
